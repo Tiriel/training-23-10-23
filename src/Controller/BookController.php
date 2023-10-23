10f4787c1e526e88725c1b0da0cc5433ee0589b3
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-10-23 17:02:50 +0200

    Twig

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,33 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/book')]
 class BookController extends AbstractController
 {
     #[Route('', name: 'app_book_index')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BookController.php',
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController::index',
         ]);
     }
 
-    #[Route('/{id<\d+>?2}',
+    #[Route('/{!id<\d+>?2}',
         name: 'app_book_show',
         requirements: ['id' => '\d+'],
         defaults: ['id' => 2],
         methods: ['GET', 'POST'],
     //condition: "request.headers.get('x-custom-header') == 'foo'"
     )]
-    public function show(int $id = 1): JsonResponse
+    public function show(int $id = 1): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BookController.php',
-            'id' => $id,
+        return $this->render('book/show.html.twig', [
+            'controller_name' => 'BookController::show - id: '.$id,
         ]);
     }
 }
