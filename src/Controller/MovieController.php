c5d4d934aa2f20568d8a4f562e962b08402d04f9
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-10-24 14:34:18 +0200

    Repositories

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,39 +12,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class MovieController extends AbstractController
 {
     #[Route('', name: 'app_movie_index')]
-    public function index(): Response
+    public function index(MovieRepository $repository): Response
     {
         return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+            'movies' => $repository->findBy([], [], 21),
         ]);
     }
 
     #[Route('/{id<\d+>}', name: 'app_movie_show')]
-    public function show(int $id): Response
+    public function show(?Movie $movie): Response
     {
-        $movie = [
-            'id' => $id,
-            'title' => 'Star Wars - Episode IV : A New Hope',
-            'country' => 'United States',
-            'releasedAt' => new \DateTimeImmutable('25-05-1977'),
-            'genres' => [
-                'Action',
-                'Adventure',
-                'Fantasy',
-            ],
-        ];
-
         return $this->render('movie/show.html.twig', [
             'movie' => $movie,
         ]);
     }
 
-    public function lastMovies(): Response
+    public function lastMovies(MovieRepository $repository): Response
     {
-        $lastMovies = [
-            ['title' => '1984'],
-            ['title' => 'The Matrix'],
-        ];
+        $lastMovies = $repository->findBy([], ['releasedAt' => 'DESC'], 10);
 
         return $this->render('includes/_last_movies.html.twig', [
             'last_movies' => $lastMovies,
