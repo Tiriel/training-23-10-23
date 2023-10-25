95f1f72f41f1bf0b4c7bbe313355f5d4873a1ad2
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-10-25 14:38:04 +0200

    Decorator

---
+++ src/Movie/Search/CacheableOmdbApiConsumer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Movie\Search;
+
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\DependencyInjection\Attribute\When;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
+#[When(env: 'prod')]
+#[AsDecorator(decorates: OmdbApiConsumer::class)]
+class CacheableOmdbApiConsumer extends OmdbApiConsumer
+{
+    public function __construct(
+        private readonly OmdbApiConsumer $inner,
+        private readonly CacheInterface $cache,
+        private readonly SluggerInterface $slugger
+    ) {}
+
+    public function fetchMovie(SearchTypes $type, string $value): array
+    {
+        $key = sprintf("%s_%s", $type->value, $this->slugger->slug($value));
+
+        return $this->cache->get(
+            $key,
+            fn() => $this->inner->fetchMovie($type, $value)
+        );
+    }
+}
