0550e955b2b745516c4f96ba1847cb51ef869134
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-10-26 12:18:11 +0200

    Birthday on users

---
+++ migrations/Version20231026101513.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231026101513 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD COLUMN birthday DATE DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, email, password, roles, preferred_channel FROM user');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , preferred_channel VARCHAR(255) DEFAULT NULL)');
+        $this->addSql('INSERT INTO user (id, email, password, roles, preferred_channel) SELECT id, email, password, roles, preferred_channel FROM __temp__user');
+        $this->addSql('DROP TABLE __temp__user');
+    }
+}
